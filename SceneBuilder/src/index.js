5eb2a6ff3a041f633b5a49cd6f2b6a8f0f7f0147
honestfreak <me21b006@iittp.ac.in>
2024-02-17 06:43:52 +0530

    add object

--- SceneBuilder/src/index.js
+++ SceneBuilder/src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './styles.css'
-import App from './App'
+// import App from './App'
+import App from './AppNew'
 
 createRoot(document.getElementById('root')).render(<App />)
