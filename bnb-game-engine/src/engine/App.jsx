aa2d1868f6d1b962c836793d16374dba766fd61e
SiddharthaG <91111745+S1ddh4rthaG@users.noreply.github.com>
2024-02-18 19:40:00 +0530

    Environment, Light and Object added to Global Context

--- bnb-game-engine/src/engine/App.jsx
+++ bnb-game-engine/src/engine/App.jsx
@@ -8,9 +8,9 @@ import { useControls } from 'leva'
 import { useGLTF, GizmoHelper, GizmoViewport, OrbitControls, Center } from '@react-three/drei'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { PivotControls } from './pivotControls/index.tsx'
+
 import objJSON from './objectMaster.json'
-import  Green  from '../Green.tsx'
-// softShadows()
+import Green from '../Green.tsx'
 
 export default function App() {
   return (
@@ -20,15 +20,12 @@ export default function App() {
   )
 }
 
-// for testing https://gateway.pinata.cloud/ipfs/Qmdq16KoUGqckw3dX8c9VzX4WAvkxfXasCQV8k7Zzc1rTr
-
 function Scene() {
   const [isGreenVisible, setIsGreenVisible] = useState(true);
 
   const ref = useRef()
-  // const { attach } = useControls({ attach: false })
   const { state, dispatch } = useContext(GlobalContext)
-  const { objectMaster } = state
+  const { objectMaster, currentObjectIdentifer } = state
 
   const Model = ({ assetLink, assetIdentifer, collision, fixed, worldMatrix }) => {
     const gltf = useLoader(GLTFLoader, assetLink);
@@ -70,15 +67,25 @@ function Scene() {
     const AddAction = {
       type: "ADD_OBJECT",
       payload: {
-        link: assetLink,
+        // Asset Information
         assetIdentifier: assetIdentifer,
         assetLink: assetLink,
+
+        // Location and Orientation
         position: new THREE.Vector3(0, 0, 0),
         quaternion: new THREE.Quaternion(0, 0, 0, 0),
         scale: new THREE.Vector3(1, 1, 1),
         worldMatrix: new THREE.Matrix4(),
-        collision: 'no', // no, yes, box, hull, trimesh (yes=box)
-        fixed: false // true, false
+
+        // State
+        fixed: false,
+        mass: 1,
+        colliders: 'no',
+
+        // Methods
+        OnClick: "",
+        OnHover: "",
+        OnCollision: ""
       }
     }
 
@@ -97,24 +104,81 @@ function Scene() {
     dispatch(DeleteAction)
   }
 
-  // Load objectMaster.json
-  const LoadObjectMaster = () => {
-    // Should be a fetch request or load on upload
+  // Change Object
+  const ChangeAction = (data) => {
+    const ChangeAction = {
+      type: "CHANGE_OBJECT",
+      payload: data
+    }
 
+    dispatch(ChangeAction)
+  }
+
+  // Change Environment
+  const ChangeEnvironment = (data) => {
+    const ChangeAction = {
+      type: "CHANGE_ENVIRONMENT",
+      payload: data
+    }
+
+    dispatch(ChangeAction)
+  }
+
+  // Add Light
+  const AddLight = (data) => {
+    const AddAction = {
+      type: "ADD_LIGHT",
+      payload: data
+    }
+
+    dispatch(AddAction)
+  }
+
+  // Delete Light
+  const DeleteLight = (data) => {
+    const DeleteAction = {
+      type: "DELETE_LIGHT",
+      payload: data
+    }
+
+    dispatch(DeleteAction)
+  }
+
+  // Change Light
+  const ChangeLight = (data) => {
+    const ChangeAction = {
+      type: "CHANGE_LIGHT",
+      payload: data
+    }
+
+    dispatch(ChangeAction)
+  }
+
+  // Load Object Master
+  const LoadObjectMaster = () => {
     objJSON.map((object) => {
       const AddAction = {
         type: "ADD_OBJECT",
         payload: {
-          asset: <Model assetIdentifer={object.assetIdentifier} assetLink={object.assetLink} />,
-          link: object.assetLink,
+          // Asset Information
           assetIdentifier: object.assetIdentifier,
           assetLink: object.assetLink,
+
+          // Location and Orientation
           position: new THREE.Vector3(object.position.x, object.position.y, object.position.z),
           quaternion: new THREE.Quaternion(object.quaternion.x, object.quaternion.y, object.quaternion.z, object.quaternion.w),
           scale: new THREE.Vector3(object.scale.x, object.scale.y, object.scale.z),
           worldMatrix: new THREE.Matrix4().fromArray(object.worldMatrix.elements),
-          collision: object.collision,
-          fixed: object.fixed
+
+          // State
+          fixed: object.fixed,
+          mass: object.mass,
+          colliders: object.colliders,
+
+          // Methods
+          OnClick: object.OnClick,
+          OnHover: object.OnHover,
+          OnCollision: object.OnCollision
         }
       }
 
@@ -126,19 +190,20 @@ function Scene() {
     <div style={{ display: 'flex', height: '100vh' }}>
       <div style={{ width: "70%" }}>
         <Canvas shadows raycaster={{ params: { Line: { threshold: 0.15 } } }} camera={{ position: [-10, 10, 10], fov: 20 }} id='objectScene'>
-          {/* Objects Append Here */}
           {
             <>
               {
                 objectMaster.map((object) => {
-                  return <Model
-                    assetIdentifer={object.assetIdentifier}
-                    assetLink={object.assetLink}
-                    collision={object.collision}
-                    fixed={object.fixed}
-                    worldMatrix={object.worldMatrix}
-                  />
-                  // return object.asset
+                  if (object.type === "object")
+                    return <Model
+                      assetIdentifer={object.assetIdentifier}
+                      assetLink={object.assetLink}
+                      collision={object.collision}
+                      fixed={object.fixed}
+                      worldMatrix={object.worldMatrix}
+                    />
+                  else
+                    return <></>
                 })
               }
             </>
@@ -168,7 +233,7 @@ function Scene() {
 
       {/* Panel */}
       <div style={{ width: "30%" }}>
-      <button onClick={() => setIsGreenVisible(!isGreenVisible)}>
+        <button onClick={() => setIsGreenVisible(!isGreenVisible)}>
           Import from Greenfield
         </button>
         {isGreenVisible && <Green />}
@@ -185,7 +250,7 @@ function Scene() {
             {/* Delete Panel */}
             <div style={{ border: '1px solid black', padding: '10px', marginBottom: "10px" }}>
               <h2>Delete Object</h2>
-              <input type='text' placeholder='Asset Identifier' onChange={(e) => setAssetIdentifer(e.target.value)} value = {assetIdentifer} />
+              <input type='text' placeholder='Asset Identifier' onChange={(e) => setAssetIdentifer(e.target.value)} value={assetIdentifer} />
               <button onClick={() => DeleteAction(assetIdentifer)}>Delete</button>
             </div>
 
@@ -206,28 +271,29 @@ function Scene() {
               <ul style={{ overflowY: "scroll", height: "40vh" }}>
                 {
                   objectMaster.map((object) => {
-                    return <div style={{ padding: "4px", border: "2px solid white" }}>
-                      <li>Asset: <span style={{ color: "red" }}>{object.assetIdentifier}</span></li>
-                      <li>Position: <ul>
-                        <li>x: <span style={{ color: "blue" }}>{object.position.x}</span></li>
-                        <li>y: <span style={{ color: "blue" }}>{object.position.y}</span></li>
-                        <li>z: <span style={{ color: "blue" }}>{object.position.z}</span></li>
-                      </ul>
-                      </li>
-                      <li>Quaternion: <ul>
-                        <li>x: <span style={{ color: "blue" }}>{object.quaternion.x}</span></li>
-                        <li>y: <span style={{ color: "blue" }}>{object.quaternion.y}</span></li>
-                        <li>z: <span style={{ color: "blue" }}>{object.quaternion.z}</span></li>
-                        <li>w: <span style={{ color: "blue" }}>{object.quaternion.w}</span></li>
-                      </ul>
-                      </li>
-                      <li>Scale:  <ul>
-                        <li>x: <span style={{ color: "blue" }}>{object.scale.x}</span></li>
-                        <li>y: <span style={{ color: "blue" }}>{object.scale.y}</span></li>
-                        <li>z: <span style={{ color: "blue" }}>{object.scale.z}</span></li>
-                      </ul>
-                      </li>
-                    </div>
+                    if (object.type === "object")
+                      return <div style={{ padding: "4px", border: "2px solid white" }}>
+                        <li>Asset: <span style={{ color: "red" }}>{object.assetIdentifier}</span></li>
+                        <li>Position: <ul>
+                          <li>x: <span style={{ color: "blue" }}>{object.position.x}</span></li>
+                          <li>y: <span style={{ color: "blue" }}>{object.position.y}</span></li>
+                          <li>z: <span style={{ color: "blue" }}>{object.position.z}</span></li>
+                        </ul>
+                        </li>
+                        <li>Quaternion: <ul>
+                          <li>x: <span style={{ color: "blue" }}>{object.quaternion.x}</span></li>
+                          <li>y: <span style={{ color: "blue" }}>{object.quaternion.y}</span></li>
+                          <li>z: <span style={{ color: "blue" }}>{object.quaternion.z}</span></li>
+                          <li>w: <span style={{ color: "blue" }}>{object.quaternion.w}</span></li>
+                        </ul>
+                        </li>
+                        <li>Scale:  <ul>
+                          <li>x: <span style={{ color: "blue" }}>{object.scale.x}</span></li>
+                          <li>y: <span style={{ color: "blue" }}>{object.scale.y}</span></li>
+                          <li>z: <span style={{ color: "blue" }}>{object.scale.z}</span></li>
+                        </ul>
+                        </li>
+                      </div>
                   })
                 }
               </ul>
--- bnb-game-engine/src/engine/GlobalContext.jsx
+++ bnb-game-engine/src/engine/GlobalContext.jsx
@@ -1,15 +1,24 @@
 import React, { createContext, useReducer } from "react";
 
 const GlobalContext = createContext();
+
+const initialEnvironment = {
+  type: "environment",
+  assetIdentifier: "world_settings",
+  gravity: 0,
+  friction: 0.5,
+  sky_color: "black",
+  ambient_light: 0.5,
+  player_speed: 10,
+  player_mass: 50,
+  player_size: 1,
+  player_jump: 0.5,
+  stars: true,
+};
+
 const initialState = {
-
   objectMaster: [
-    {
-      "assetIdentifier": "Environment_Game_Engine",
-      "gravity": 9.8,
-      "flycontrol": false,
-  },
-
+    initialEnvironment,
   ],
   currentObjectIdentifier: "",
 };
@@ -17,24 +26,49 @@ const initialState = {
 // asset is redundant :(, only worldMatrix, assetID, assetLink are enough
 const reducer = (state, action) => {
   switch (action.type) {
+    // CASE: OBJECT
     case "ADD_OBJECT":
+      if (state.objectMaster.some((object) => object.assetIdentifier === action.payload.assetIdentifier)) {
+        alert("Object with the same identifier already exists!");
+        return state;
+      }
+
       return {
         ...state,
         objectMaster: [
           ...state.objectMaster,
           {
+            // Asset Information
+            type: "object",
             assetIdentifier: action.payload.assetIdentifier,
             assetLink: action.payload.assetLink,
+
+            // Location and Orientation
             position: action.payload.position,
             quaternion: action.payload.quaternion,
             scale: action.payload.scale,
             worldMatrix: action.payload.worldMatrix,
+
+            // State
             fixed: action.payload.fixed,
+            mass: action.payload.mass,
+            colliders: action.payload.colliders,
+
+            // Methods
+            OnClick: action.payload.OnClick,
+            OnHover: action.payload.OnHover,
+            OnCollision: action.payload.OnCollision,
           },
         ],
       };
 
     case "DELETE_OBJECT":
+      // If the object does not exist, do nothing
+      if (!state.objectMaster.some((object) => object.assetIdentifier === action.payload.assetIdentifier)) {
+        alert("Object with the given identifier does not exist!");
+        return state;
+      }
+
       return {
         ...state,
         objectMaster: state.objectMaster.filter(
@@ -43,16 +77,36 @@ const reducer = (state, action) => {
       };
 
     case "CHANGE_OBJECT":
+      if (!state.objectMaster.some((object) => object.assetIdentifier === action.payload.assetIdentifier)) {
+        alert("Object with the given identifier does not exist!");
+        return state;
+      }
+
       return {
         ...state,
         objectMaster: state.objectMaster.map((object) => {
-          if (object.assetIdentifier === action.payload.assetIdentifier) {
+          if (object.assetIdentifier === action.payload.assetIdentifier && object.type === "object") {
             return {
               ...object,
-              position: action.payload.position,
-              quaternion: action.payload.quaternion,
-              scale: action.payload.scale,
-              worldMatrix: action.payload.worldMatrix,
+              // Asset Information
+              assetIdentifier: action.payload.assetIdentifier || object.assetIdentifier,
+              assetLink: action.payload.assetLink || object.assetLink,
+
+              // Location and Orientation
+              position: action.payload.position || object.position,
+              quaternion: action.payload.quaternion || object.quaternion,
+              scale: action.payload.scale || object.scale,
+              worldMatrix: action.payload.worldMatrix || object.worldMatrix,
+
+              // State
+              fixed: action.payload.fixed || object.fixed,
+              mass: action.payload.mass || object.mass,
+              colliders: action.payload.colliders || object.colliders,
+
+              // Methods
+              OnClick: action.payload.OnClick || object.OnClick,
+              OnHover: action.payload.OnHover || object.OnHover,
+              OnCollision: action.payload.OnCollision || object.OnCollision,
             };
           }
 
@@ -60,7 +114,76 @@ const reducer = (state, action) => {
         }),
       };
 
-    
+    // CASE: ENVIRONMENT
+    case "CHANGE_ENVIRONMENT":
+      return {
+        ...state,
+        objectMaster: state.objectMaster.map((object) => {
+          if (object.assetIdentifier === "world_settings" && object.type === "environment") {
+            return {
+              ...object,
+              gravity: action.payload.gravity || object.gravity,
+              friction: action.payload.friction || object.friction,
+              sky_color: action.payload.sky_color || object.sky_color,
+              ambient_light: action.payload.ambient_light || object.ambient_light,
+              player_speed: action.payload.player_speed || object.player_speed,
+              player_mass: action.payload.player_mass || object.player_mass,
+              player_size: action.payload.player_size || object.player_size,
+              player_jump: action.payload.player_jump || object.player_jump,
+              stars: action.payload.stars || object.stars,
+            };
+          }
+
+          return object;
+        }),
+      };
+
+    // CASE: LIGHT
+    case "ADD_LIGHT":
+      return {
+        ...state,
+        objectMaster: [
+          ...state.objectMaster,
+          {
+            type: "light",
+            assetIdentifier: action.payload.assetIdentifier,
+            position: action.payload.position,
+            color: action.payload.color,
+            intensity: action.payload.intensity,
+            distance: action.payload.distance,
+            decay: action.payload.decay,
+            shadow: action.payload.shadow,
+          },
+        ],
+      };
+
+    case "DELETE_LIGHT":
+      return {
+        ...state,
+        objectMaster: state.objectMaster.filter(
+          (object) => object.assetIdentifier !== action.payload.assetIdentifier
+        ),
+      };
+
+    case "CHANGE_LIGHT":
+      return {
+        ...state,
+        objectMaster: state.objectMaster.map((object) => {
+          if (object.assetIdentifier === action.payload.assetIdentifier && object.type === "light") {
+            return {
+              ...object,
+              position: action.payload.position || object.position,
+              color: action.payload.color || object.color,
+              intensity: action.payload.intensity || object.intensity,
+              distance: action.payload.distance || object.distance,
+              decay: action.payload.decay || object.decay,
+              shadow: action.payload.shadow || object.shadow,
+            };
+          }
+
+          return object;
+        }),
+      };
 
     case "SET_CURRENT_OBJECT":
       return {
